586231ef8601825a85aaa8ac5765a2676b2472fe
Indah Putri Herni <indahputriherni@gmail.com>
2021-09-22 14:53:17 +0700

    Pembuatan web PHP Object Oriented

---
+++ tugas1.php
@@ -0,0 +1,74 @@
+<?php
+
+echo "Animal <br>";
+echo "<hr>";
+
+
+class kucing {
+	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$kucing = new kucing;
+$kucing->nama = "Bonbon";
+$kucing->jumlah_kaki = "4";
+$kucing->bisa_terbang = "Tidak";
+$kucing->suara = "Meooonggg";
+
+echo "kucing <br>";
+echo "Kucing saya adalah $kucing->nama <br>";
+echo "Jumlah kaki : $kucing->jumlah_kaki <br>";
+echo "Bisa Terbang : $kucing->bisa_terbang <br>";
+echo "Suara : $kucing->suara <br>";
+echo "<hr>";
+
+class ayam {
+	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$ayam = new ayam;
+$ayam->nama = "Pakcoy";
+$ayam->jumlah_kaki = "2";
+$ayam->bisa_terbang = "Tidak";
+$ayam->suara = "kukkuruyukkkk";
+
+echo "ayam <br>";
+echo "ayam saya adalah $ayam->nama <br>";
+echo "Jumlah kaki : $ayam->jumlah_kaki <br>";
+echo "Bisa Terbang : $ayam->bisa_terbang <br>";
+echo "Suara : $ayam->suara <br>";
+echo "<hr>";
+
+class jangkrik {
+	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$jangkrik = new jangkrik;
+$jangkrik->nama = "Cio";
+$jangkrik->jumlah_kaki = "6";
+$jangkrik->bisa_terbang = "Bisa";
+$jangkrik->suara = "Krik krik krik krik";
+
+echo "jangkrik <br>";
+echo "jangkrik saya adalah $jangkrik->nama <br>";
+echo "Jumlah kaki : $jangkrik->jumlah_kaki <br>";
+echo "Bisa Terbang : $jangkrik->bisa_terbang <br>";
+echo "Suara : $jangkrik->suara <br>";
+echo "<hr>";
+
+class serigala {
+	public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+}
+
+$serigala = new serigala;
+$serigala->nama = "Ben";
+$serigala->jumlah_kaki = "4";
+$serigala->bisa_terbang = "Tidak bisa";
+$serigala->suara = "Aunggggggg";
+
+echo "serigala <br>";
+echo "Serigala saya adalah $serigala->nama <br>";
+echo "Jumlah kaki : $serigala->jumlah_kaki <br>";
+echo "Bisa Terbang : $serigala->bisa_terbang <br>";
+echo "Suara : $serigala->suara <br>";
+echo "<hr>";
+
